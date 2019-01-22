ced38e7027c8e8d3308f8da77665b65622e5b66d
Chong Wang <chong1.wang@valeo.com>
2019-01-22 11:49:12 -0500

    second change

--- i2c-core.c
+++ i2c-core.c
@@ -1,4 +1,5 @@
 //first changes
+//second changes
 /* i2c-core.c - a device driver for the iic-bus interface		     */
 /* ------------------------------------------------------------------------- */
 /*   Copyright (C) 1995-99 Simon G. Vogl
